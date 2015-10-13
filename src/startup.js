c1753faf6d6d53c750dbffd3a56abfe9de0fc8c1
Vassilis Vatikiotis <vvatikiotis@gmail.com>
2015-10-13 12:41:55 +0300

    Initial commit

---
+++ src/startup.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import './styles/main.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/app';
+
+var element = React.createElement(App, {});
+ReactDOM.render(element, document.querySelector('#app-root'));
